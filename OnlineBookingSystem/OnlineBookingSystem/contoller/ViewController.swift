7273eb5122d5442edfaf988bfec6c88203a5aea8
Alex <522892542@qq.com>
2022-05-06 09:41:31 +0800

    init

---
+++ OnlineBookingSystem/OnlineBookingSystem/contoller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineBookingSystem
+//
+//  Created by 刘阳 on 2022/5/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
